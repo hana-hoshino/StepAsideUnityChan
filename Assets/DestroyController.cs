d9f6ac6cbaa57253acd0a4ca3c78b390a86c80d6
Kana.H <kirakishi20@gmail.com>
2020-04-26 18:16:38 +0900

    Lesson7課題です 途中経過です

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//this.transformで自分自身の位置取得、カメラの位置より小さくなった場合にGameObjectDestroyを呼ぶ
+		if (this.transform.position.z < Camera.main.gameObject.transform.position.z)
+        {
+			//カメラに写っていないオブジェクトを消す
+			Debug.Log("カメラに写っていないオブジェクトが消えました");
+			GameObject.Destroy(this.gameObject);
+
+		}
+
+	}
+
+}
